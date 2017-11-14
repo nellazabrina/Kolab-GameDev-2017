393bfb49b545bc9202ac6fd27a9fc47f688af351
Dasper <buddy90_lost@yahoo.co.id>
2017-11-14 00:45:17 +0000

    unit.h created online with Bitbucket

---
+++ unit.h
@@ -0,0 +1,50 @@
+#ifndef _UNIT_H
+#define _UNIT_H
+
+#include "point.h"
+#include "mesinkata.h"
+#include "listsirkuler.h"
+
+const Kata range("Range", 5);
+const Kata melle("Melle", 5);
+const Kata endunit("End", 3);
+
+typedef struct{
+	Kata type;
+	Kata atktype;
+	boolean canatk;
+	int maxhealth;
+	int currhealth;
+	int atkdmg;
+	int maxmove;
+	int currmove;
+	int heal;
+	int cost;
+	Point pos;
+} Unit;
+
+typedef struct {
+	char CC;
+	int kepemilikan;
+} CharUnit;
+
+#define atktype(X) (X).atktype
+#define canatk(X) (X).canatk
+#define health(X) (X).health
+#define atkdmg(X) (X).atkdmg
+#define cost(X) (X).cost
+#define heal(X) (X).heal
+#define currmove(X) (X).currmove
+#define position(X) (X).pos
+#define pemilik(X) (X).kepemilikan
+#define CC(X) (X).CC
+
+void bacaunit(LIST *stdunit);
+
+void PrintUnitRekrut(Unit U);
+
+void PrintMove(Unit U);
+
+void PrintUnit(Unit U);
+
+#endif
