a9e90b61da2a04577f7c940c9a95da12bdce969b
Muh Habibi Haidir <muhhabibih@gmail.com>
2017-11-16 10:13:50 +0000

    map.c created online with Bitbucket

---
+++ map.c
@@ -0,0 +1,68 @@
+#include "map.h"
+
+void CreateMap(int m, int n, MATRIKS* M) {
+  MakeMATRIKS(n,m,M);
+  for (int i=0; i<m; i++) {
+    for (int j=0; j<n; j++) {
+      if (i%4==0) {
+          Elmt(*M,i,j).CC='*';
+	  Elmt(*M,i,j).kepemilikan=3;
+      }
+      else {
+        if (j%4==0) {
+          Elmt(*M,i,j).CC='*';
+	  Elmt(*M,i,j).kepemilikan=3;
+        }
+        else {
+          Elmt(*M,i,j).CC=' ';
+	  Elmt(*M,i,j).kepemilikan=3;
+        }
+      }
+    }
+  }
+  PrintMap(m, n, *M);
+}
+
+void PrintMap(int m, int n, MATRIKS M) {
+  for (int k=0; k<(n-1)/4; k++) {
+    if (k==0) {
+        printf("   ");
+    }
+    printf("  ");
+    printf("%d", k);
+    if (k==((n-1)/4)-1) {
+      printf("\n");
+    }
+    else {
+      if (k<10) {
+        printf(" ");
+      }
+    }
+  }
+
+  int l = 0;
+
+  for (int i=0; i<m; i++) {
+    for (int j=0; j<n; j++) {
+      if (j==0) {
+        if ((i-2)%4==0) {
+          if (l<10) {
+            printf(" %d ", l);
+          }
+          else {
+            printf("%d ", l);
+          }
+          l++;
+        }
+        else {
+          printf("   ");
+        }
+      }
+
+      print_char(Elmt(M, i, j)); //printf("%c", Elmt(M, i, j));
+      if (j==(n-1)) {
+        printf("\n");
+      }
+    }
+  }
+}
