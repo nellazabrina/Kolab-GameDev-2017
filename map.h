69590233c906a5cfcc7c986bbfdd056bf2278a16
nellazabrina <nellazabrinapramata@yahoo.co.id>
2017-11-16 18:54:40 +0700

    Update map.h

--- map.h
+++ map.h
@@ -11,6 +11,12 @@ void PrintMap(int m, int n, MATRIKS M);
 /* Untuk Mencetak Map */
 
 void convertIndex (int *m, int *n);
-/* Untuk mengkonvert indeks */
+/* Untuk mengkonvert indeks pertama di petak */
+
+void IndexUnit (int *m, int *n);
+/* Untuk mengakses indeks unit */
+
+void IndexBuilding (int *m, int *n);
+/* Untik mengakses indeks building */
 
 #endif
