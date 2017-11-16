21efdd4fafa6201725d35b84f30b0e9748f09a4a
Muh Habibi Haidir <muhhabibih@gmail.com>
2017-11-16 10:13:02 +0000

    map.h created online with Bitbucket

---
+++ map.h
@@ -0,0 +1,12 @@
+#include "matriks.h"
+
+#ifndef MAP_H
+#define MAP_H
+
+void CreateMap(int m, int n, MATRIKS* M);
+/* Untuk membuat map Awal */
+
+void PrintMap(int m, int n, MATRIKS M);
+/* Untuk Mencetak Map */
+
+#endif
